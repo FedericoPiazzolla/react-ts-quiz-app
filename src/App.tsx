b342a8c16a48d5101c42f53f9579578fb7267fba
FedericoPiazzolla <Federicop257@gmail.com>
2025-09-08 20:00:49 +0200

    add: setup useState in app.tsx for props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 //Components
 import QuestionCard from "./components/QuestionCard";
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
-  const startTrivia = async () => {
-    console.log("start");
-  };
+  const [isLoading, setIsloading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("check");
-  };
+  const startTrivia = async () => {};
 
-  const nextQuestion = () => {
-    console.log("next");
-  };
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
 
   return (
     <div className="App">
@@ -24,7 +27,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
